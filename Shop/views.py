9cf810b3f6023bb940f3d29cd9daa3a500542e6f
Rahul Golani <rahulgolani24@live.com>
2020-05-23 01:39:36 +0530

    Added Bootstrap

--- Shop/views.py
+++ Shop/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Shop Home")
+    return render(request,'Shop/index.html')
