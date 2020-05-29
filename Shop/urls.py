a9b5bdd4493507f0d3ec021c5b245e336ad83945
Rahul Golani <rahulgolani24@live.com>
2020-05-29 17:14:26 +0530

    Pipeline

--- Shop/urls.py
+++ Shop/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='ShopHome'),
+    path('about/', views.about,name='AboutUs'),
+    path('contact/', views.contact,name='ContactUs'),
+    path('tracker/', views.tracker,name='TrackingStatus'),
+    path('productView/', views.productView,name='Product'),
+    path('search/', views.search,name='Search'),
+    path('checkout/', views.checkout,name='Checkout'),
+
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'Shop/index.html')
+
+def about(request):
+    return HttpResponse("We are at About")
+
+def contact(request):
+    return HttpResponse("We are at Contact")
+
+def tracker(request):
+    return HttpResponse("We are at Tracker")
+
+def productView(request):
+    return HttpResponse("We are at ProductView")
+
+def search(request):
+    return HttpResponse("We are at Search")
+
+def checkout(request):
+    return HttpResponse("We are at Checkout")
